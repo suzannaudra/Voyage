dcd33a4258bda561dba50ccf98b261529bdf8b8c
David Flores <drfloresj@gmail.com>
2020-03-09 21:10:35 -0700

    adding folders, voyage-app.js file, script references on index.html

---
+++ assets/javascript/voyage-app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var firebaseConfig = {
+    // TODO setup firebase and copy config parameters here
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// VARIABLES
+
+// FUNCTIONS
+
+// INITIALIZE/MAIN
