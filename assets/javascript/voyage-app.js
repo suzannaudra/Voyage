5c8aa0165b095f1146bc8e32621fd16f8de53b5a
David Flores <drfloresj@gmail.com>
2020-03-11 21:30:08 -0700

    survey scoring logic starter code

--- assets/javascript/voyage-app.js
+++ assets/javascript/voyage-app.js
@@ -7,7 +7,115 @@ firebase.initializeApp(firebaseConfig);
 
 
 // VARIABLES
+var Vail = 0;
+var Vienna = 0;
+var SanDiego = 0;
+var Bahamas = 0;
+var Alaska = 0;
+var something = 0; // just for testing
+
+var arrDestinations = [Vail, Vienna, SanDiego, Bahamas, Alaska];
+
 
 // FUNCTIONS
+function checkSurveyRadioButtons() {
+    // console.log("checked: ", $("#q1-adventure").is(':checked'));
+    if ($("#q1-adventure").is(':checked')) {
+        Vail++;
+    };
+    if ($("#q1-relaxation").is(':checked')) {
+        Bahamas++;
+    };
+    if ($("#q1-combo").is(':checked')) {
+        Vienna++;
+    };
 
-// INITIALIZE/MAIN
+    if ($("#q2-sunshine").is(':checked')) {
+        Bahamas++;
+    };
+    if ($("#q2-snow").is(':checked')) {
+        Alaska++;
+        Vail++;
+    };
+    if ($("#q2-combo").is(':checked')) {
+        Vail++;
+    };
+
+    if ($("#q3-beach").is(':checked')) {
+        SanDiego++;
+        Bahamas++;
+    };
+    if ($("#q3-skislopes").is(':checked')) {
+        Vail++;
+        Alaska++;
+    };
+    if ($("#q3-hiking").is(':checked')) {
+        Vail++;
+        Alaska++;
+        SanDiego++;
+    };
+    if ($("#q3-explorecity").is(':checked')) {
+        SanDiego++;
+        Vienna++;
+    };
+
+    if ($("#q4-historical").is(':checked')) {
+        Vienna++;
+        SanDiego++;
+    };
+    if ($("#q4-atv").is(':checked')) {
+        Alaska++;
+    };
+    if ($("#q4-pool").is(':checked')) {
+        SanDiego++;
+        Bahamas++;
+    };
+    if ($("#q4-nightlife").is(':checked')) {
+        Vienna++;
+        SanDiego++;
+    };
+
+    if ($("#q5-shortflight").is(':checked')) {
+        SanDiego++
+    };
+    if ($("#q5-acrosscountry").is(':checked')) {
+        Alaska++;
+        Vail++;
+    };
+    if ($("#q5-acrossworld").is(':checked')) {
+        Vienna++;
+    };
+
+    // TODO: push each destination variable into array
+
+    // TODO: sort array large to small to find winner/runner up
+    
+    // TODO: call function (need to create it) that displays travel destination stuff!
+
+
+
+}
+
+
+
+$(document).ready(function () {
+    $('.surveyquestions').hide();
+
+});
+
+
+
+// Hides start button and title after clicking start
+$('#pstart').on('click', function () {
+    $('#start').remove();
+    $('#heading').remove();
+    $('.surveyquestions').show();
+    $('#pstart').remove();
+});
+
+$('#test-button').on('click', function () {
+    checkSurveyRadioButtons();
+});
+
+
+// INITIALIZE/MAIN 
