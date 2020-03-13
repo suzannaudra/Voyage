21c027097b68fe3f29751049f2edff811ea0a3a3
David Flores <drfloresj@gmail.com>
2020-03-12 21:40:26 -0700

    reworked scoring and ranking variables, added plenty of comments :)

--- assets/javascript/voyage-app.js
+++ assets/javascript/voyage-app.js
@@ -1,96 +1,98 @@
 // Initialize Firebase
 var firebaseConfig = {
-    // TODO setup firebase and copy config parameters here
+    apiKey: "AIzaSyA7yTHFyRl7s6ZvTvQ-chYiE5flrw8NJ0g",
+    authDomain: "myapp-654ae.firebaseapp.com",
+    databaseURL: "https://myapp-654ae.firebaseio.com",
+    projectId: "myapp-654ae",
+    storageBucket: "myapp-654ae.appspot.com",
+    messagingSenderId: "437569120119",
+    appId: "1:437569120119:web:3d608b52341c7f06806372",
+    measurementId: "G-G5YVGZ0V28"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 
 // VARIABLES
-var Vail = 1;
-var Vienna = 2;
-var SanDiego = 3;
-var Bahamas = 4;
-var Alaska = 5;
-var something = 0; // just for testing
 
-var arrDestinations = [Vail, Vienna, SanDiego, Bahamas, Alaska];
+// webcam api key (owner: Thuy)
+var webcamApiKey = "QwMvscmcOAV4Xsn2Hr6N9MNJ1dGAGGLO";
+
+// save destination vars in object for sorting/ranking
+var destinations = {
+    Vail:       1,
+    Vienna:     2,
+    SanDiego:   5,
+    Bahamas:    4,
+    Alaska:     3
+};
 
 
 // FUNCTIONS
 function checkSurveyRadioButtons() {
-    // console.log("checked: ", $("#q1-adventure").is(':checked'));
-    if ($("#q1-adventure").is(':checked')) {
-        Vail++;
-    };
-    if ($("#q1-relaxation").is(':checked')) {
-        Bahamas++;
-    };
-    if ($("#q1-combo").is(':checked')) {
-        Vienna++;
-    };
+    
+    // tally the survey results into destination vars
+    if ($("#q1-adventure").is(':checked')) {destinations.Vail++;};
+    if ($("#q1-relaxation").is(':checked')) {destinations.Bahamas++;};
+    if ($("#q1-combo").is(':checked')) {destinations.Vienna++;};
 
-    if ($("#q2-sunshine").is(':checked')) {
-        Bahamas++;
+    if ($("#q2-sunshine").is(':checked')) {destinations.Bahamas++;};
+    if ($("#q2-snow").is(':checked')){
+        destinations.Alaska++;
+        destinations.Vail++;
     };
-    if ($("#q2-snow").is(':checked')) {
-        Alaska++;
-        Vail++;
-    };
-    if ($("#q2-combo").is(':checked')) {
-        Vail++;
-    };
+    if ($("#q2-combo").is(':checked')) {destinations.Vail++;};
 
     if ($("#q3-beach").is(':checked')) {
-        SanDiego++;
-        Bahamas++;
+        destinations.SanDiego++;
+        destinations.Bahamas++;
     };
     if ($("#q3-skislopes").is(':checked')) {
-        Vail++;
-        Alaska++;
+        destinations.Vail++;
+        destinations.Alaska++;
     };
     if ($("#q3-hiking").is(':checked')) {
-        Vail++;
-        Alaska++;
-        SanDiego++;
+        destinations.Vail++;
+        destinations.Alaska++;
+        destinations.SanDiego++;
     };
     if ($("#q3-explorecity").is(':checked')) {
-        SanDiego++;
-        Vienna++;
+        destinations.SanDiego++;
+        destinations.Vienna++;
     };
-
     if ($("#q4-historical").is(':checked')) {
-        Vienna++;
-        SanDiego++;
+        destinations.Vienna++;
+        destinations.SanDiego++;
     };
     if ($("#q4-atv").is(':checked')) {
-        Alaska++;
+        destinations.Alaska++;
     };
     if ($("#q4-pool").is(':checked')) {
-        SanDiego++;
-        Bahamas++;
+        destinations.SanDiego++;
+        destinations.Bahamas++;
     };
     if ($("#q4-nightlife").is(':checked')) {
-        Vienna++;
-        SanDiego++;
+        destinations.Vienna++;
+        destinations.SanDiego++;
     };
-
-    if ($("#q5-shortflight").is(':checked')) {
-        SanDiego++
-    };
+    if ($("#q5-shortflight").is(':checked')) {destinations.SanDiego++};
     if ($("#q5-acrosscountry").is(':checked')) {
-        Alaska++;
-        Vail++;
+        destinations.Alaska++;
+        destinations.Vail++;
     };
-    if ($("#q5-acrossworld").is(':checked')) Vienna++;
-
-    // TODO: push each destination variable into array
-
-    // TODO: sort array large to small to find winner/runner up
-    arrDestinations.sort(); // sorts the array in ascending order
-    arrDestinations.reverse(); // then sorts in decending order (highest first)
-    for (var a of arrDestinations) {
-        console.log(a);
+    if ($("#q5-acrossworld").is(':checked')) {destinations.Vienna++;}
+    // Sorting the scores to rank the destinations...
+    //  Object.entries saves each destination:score (key:value) pair into 
+    //  an array so we can sort by the score values (sorting objects by their properties
+    //  requires a little extra effort)
+    let toBeSorted = Object.entries(destinations);
+        // example: toBeSorted = [ ["Bahamas", 4], ["Vail", 5]...]
+    let sorted = toBeSorted.sort(function(x, y){return y[1] - x[1]}); // sorts the score values in decending order (rank high-low)
+        // example: sorted = [ ["Vail", 5], ["Bahamas",4]...]
+    for (var i of sorted) {
+        // i = each [destination, score] array inside the "sorted" array
+        console.log(i[0], i[1]); // displays destination name (index 0) and score (index 1)
+        if(sorted.indexOf(i)+1 === 2){break;} // breaks the loop after top two destinations: winner & runner up
     }
 
     // TODO: call function (need to create it) that displays travel destination stuff!
